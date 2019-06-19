d0c2e720273dce6213abb71df1fff55f69dbc441
wolfcoming1121 <18600887431@163.com>
2019-06-19 16:29:46 +0800

    gradle

--- learncomponent/src/main/java/com/example/learncomponent/activity/ShipeiActivity.kt
+++ learncomponent/src/main/java/com/example/learncomponent/activity/ShipeiActivity.kt
@@ -1,25 +1,37 @@
 package com.example.learncomponent.activity
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.didichuxing.doraemonkit.util.SharedPrefsUtil
 import com.example.learncomponent.R
 import com.infoholdcity.basearchitecture.self_extends.toast
 import com.infoholdcity.baselibrary.base.BaseActiviy
+import com.infoholdcity.baselibrary.utils.FileUtils
+import com.infoholdcity.baselibrary.utils.SPUtils
 import kotlinx.android.synthetic.main.activity_shipei.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-class ShipeiActivity:BaseActiviy() {
+class ShipeiActivity : BaseActiviy() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shipei)
         save1.setOnClickListener {
-            toast("save1")
+            SPUtils.getInstance(this).put("name2","yqqq")
+            SharedPrefsUtil.putString(this, "name", "yyyy")
         }
 
         save2.setOnClickListener {
-
+            FileUtils.writeFileToSDCardAppend("添加内容","1测试Q","aa.txt")
         }
 
         btnget.setOnClickListener {
-            toast("get")
+            val string = SharedPrefsUtil.getString(this, "name", "null")
+            toast(string)
         }
+
     }
+
+
 }
